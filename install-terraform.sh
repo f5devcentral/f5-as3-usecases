848dd2effd74fd221a64fd1bdae0b77a8d5be8c9
Ubuntu <scshitole@gmail.com>
2020-01-27 19:12:29 +0000

    update

--- install-terraform.sh
+++ install-terraform.sh
@@ -3,6 +3,8 @@ sudo apt update
 sudo apt install jq
 set -e
 sudo apt-get install unzip
+cd ..
+mv terraform.tfvars ~/f5-as3-usecases
 cd /tmp
 wget https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
 unzip terraform_0.12.20_linux_amd64.zip 
