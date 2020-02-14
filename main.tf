5e99965f796b1be7e8dbae6b0a6912256b3cfa94
Sanjay Shitole <scshitole>
2020-02-13 19:28:52 -0800

    added asm

--- main.tf
+++ main.tf
@@ -22,6 +22,17 @@ resource "null_resource" "install_as3" {
 # deploy application using as3
 resource "bigip_as3" "nginx" {
   as3_json    = file("example1.json")
-  config_name = "demo"
+  config_name = "Sample_new"
   depends_on  = [null_resource.install_as3]
+}
+
+# Enable ASM module on BIG-IP
+resource "bigip_sys_provision" "asm" {
+  name        = "asm"
+  full_path    = "/Common/asm"
+  cpu_ratio    = 0
+  disk_ratio   = 0
+  level       = "nominal"
+  memory_ratio = 0
+  depends_on = [bigip_as3.nginx]
 }
